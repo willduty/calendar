035270091b0691124447d0702c6bfd1a0d94e24f
will duty <willduty@yahoo.com>
2012-04-24 21:00:48 -0400

    testauthtrycatch4

--- app/Config/core.php
+++ app/Config/core.php
@@ -32,7 +32,7 @@
  * In production mode, flash messages redirect after a time interval.
  * In development mode, you need to click the flash message to continue.
  */
-	Configure::write('debug', 0);
+	Configure::write('debug', 2);
 
 /**
  * Configure the Error handler used to handle errors for your application.  By default
@@ -190,7 +190,6 @@
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
 	Configure::write('Security.cipherSeed', '845693069840536495804958420384759287');
-
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
