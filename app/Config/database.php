8aea9a41fa9f684c68d46d7870d9ffb9930a9aaf
will duty <willduty@yahoo.com>
2012-04-26 11:13:53 -0400

    test email format 7

--- app/Config/database.php
+++ app/Config/database.php
@@ -58,7 +58,7 @@
  * For MySQL to connect via socket specify the `unix_socket` parameter instead of `host` and `port`
  */
 class DATABASE_CONFIG {
-/*
+
 	public $default = array(
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
@@ -69,8 +69,8 @@ class DATABASE_CONFIG {
 		'prefix' => '',
 		//'encoding' => 'utf8',
 	);
-	*/
-	public $default = array(
+	
+/*	public $default = array(
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
 		'host' => 'localhost',
@@ -80,7 +80,7 @@ class DATABASE_CONFIG {
 		'prefix' => '',
 		//'encoding' => 'utf8',
 	);
-	
+	*/
 
 	public $test = array(
 		'datasource' => 'Database/Mysql',
